4c00d43c3361fbcd0f025fee60117b0f5505fb7d
leo9224 <leofetter92@gmail.com>
2024-05-14 15:57:07 +0200

    posibility to create park

---
+++ DotNetProjectBlazor/Pages/ParkFormPage.razor.cs
@@ -0,0 +1,58 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using DotNetProjectLibrary.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Newtonsoft.Json;
+
+namespace DotNetProjectBlazor.Pages
+{
+    partial class ParkFormPage
+    {
+        [Parameter]
+        public int UserId { get; set; }
+        [Parameter]
+        public int ParkId { get; set; }
+        public Park? Park { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (ParkId == -1)
+            {
+                Park = new Park() { name = ""};
+            }
+            else
+            {
+                HttpClient httpClient = new HttpClient();
+                //httpClient.DefaultRequestHeaders.Add("Authorization", Token);
+                HttpResponseMessage httpResponseMessage = await httpClient.GetAsync($"{Config.APIEndpoint}/api/park/{ParkId}");
+
+                Park = await httpResponseMessage.Content.ReadFromJsonAsync<Park>();
+            }
+        }
+
+        public async void SubmitForm(MouseEventArgs args)
+        {
+            HttpClient httpClient = new HttpClient();
+            //httpClient.DefaultRequestHeaders.Add("Authorization", Token);
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(Park), Encoding.UTF8, MediaTypeHeaderValue.Parse("application/json"));
+
+
+            if (ParkId == -1)
+            {
+                HttpResponseMessage httpResponseMessage = await httpClient.PostAsync($"{Config.APIEndpoint}/api/park", content);
+                Park? park = await httpResponseMessage.Content.ReadFromJsonAsync<Park>();
+
+                HttpContent content2 = new StringContent($"{{\"user_id\":{UserId},\"park_id\":{park.id}}}", Encoding.UTF8, MediaTypeHeaderValue.Parse("application/json"));
+                HttpResponseMessage httpResponseMessage2 = await httpClient.PostAsync($"{Config.APIEndpoint}/api/user_park", content2);
+            }
+            else
+            {
+                HttpResponseMessage httpResponseMessage = await httpClient.PutAsync($"{Config.APIEndpoint}/api/park/{ParkId}", content);
+            }
+
+            NavigationManager.NavigateTo($"/{UserId}");
+        }
+    }
+}
