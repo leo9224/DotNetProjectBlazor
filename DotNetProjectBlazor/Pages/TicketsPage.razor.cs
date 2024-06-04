7fe0f09cd370da0fbfebe51d91560fabcb1a321e
leo9224 <leofetter92@gmail.com>
2024-06-04 15:33:39 +0200

    add ticket page

---
+++ DotNetProjectBlazor/Pages/TicketsPage.razor.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Http.Json;
+using DotNetProjectLibrary.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace DotNetProjectBlazor.Pages
+{
+    partial class TicketsPage
+    {
+        [Parameter]
+        public int UserId { get; set; }
+        private List<Ticket> Tickets = new List<Ticket>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("Authorization", await LocalStorage.GetItemAsStringAsync("Token"));
+            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync($"{Config.APIEndpoint}/api/ticket");
+
+            IEnumerable<Ticket>? tickets = await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<Ticket>>();
+
+            if (tickets != null && tickets?.Count() != 0)
+            {
+                Tickets = tickets.ToList();
+            }
+        }
+
+        public async void DeleteTicket(int id)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("Authorization", await LocalStorage.GetItemAsStringAsync("Token"));
+            HttpResponseMessage httpResponseMessage = await httpClient.DeleteAsync($"{Config.APIEndpoint}/api/ticket/{id}");
+
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                NavigationManager.NavigateTo(NavigationManager.Uri, true);
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
